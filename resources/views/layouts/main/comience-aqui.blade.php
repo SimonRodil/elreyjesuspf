bb1e7ff45b9740a2dc8c04222e7e7e7ebe90fcc9
Simon Rodil <rodileduar.simon@gmail.com>
2021-07-08 13:34:34 -0400

    Mejoras finales

---
+++ resources/views/layouts/main/comience-aqui.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.main.app')
+
+@section('content')
+
+<section class="hero-wrap hero-wrap-2" style="background-image: url('assets_main/images/iam-new-here.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-end justify-content-center">
+          <div class="col-md-9 ftco-animate pb-5 text-center">
+            <h1 class="mb-3 bread">Comience Aquí</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.php">Inicio <i class="ion-ios-arrow-forward"></i></a></span> <span>Comience Aquí <i class="ion-ios-arrow-forward"></i></span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+   	
+    <section class="ftco-section">
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-md-12 mb-5">
+						<h2>¡Bienvenido a El Rey Jesús Punto Fijo!</h2>
+						<p>Somos una iglesia en la ciudad de Punto Fijo, Venezuela, donde usted y su familia pueden experimentar un encuentro con Dios y Su milagroso amor y poder. Sabemos que Dios tiene un propósito divino para su vida y estamos aquí para ayudarlo a descubrirlo, desarrollarlo y caminar en él. Dios nos ha llamado a traer su poder sobrenatural a esta generación. Esto simplemente significa que creemos que Dios es omnisciente y todopoderoso y que nada, absolutamente nada es imposible para Él. Así que cualquiera que sea la circunstancia que usted pueda estar pasando en su vida usted siempre tendrá un lugar donde puede venir esperando recibir un rompimiento. Por esa razón siempre decimos, “¡Aquí Ocurren Milagros!”</p>
+    			</div>
+    			<div class="col-md-6 d-flex">
+						<img src="images/pastores-aboutus.jpg" class="img-responsive" style="width:100%">
+					</div>
+    			<div class="col-md-6">
+						<h2>¡Conoce a nuestros pastores, mentores y lideres territoriales!</h2>
+						<p>La Pastora Gisela Revilla de Bracho, forma parte de la familia del Ministerio Internacional El Rey Jesús, desde 2011. Ellos han servido a Dios activamente en diferentes departamentos y ministerios. En el año 2009 fué ordenada como pastora. Tiene un fuerte llamado a formar una nueva generación de líderes con principios y valores de fe para así transformar la cultura de una ciudad y una nación</p>
+    			</div>
+        </div>
+    	</div>
+    </section>
+		
+		<section class="ftco-casadepaz ftco-section img" style="background-image: url(assets_main/images/pareja.jpg); height: 100%">
+			<div class="overlay"></div>
+    	<div class="container">
+    		<div class="row d-md-flex justify-content-end">
+    			<div class="col-md-6 half p-3 p-md-5 ftco-animate heading-section">
+    				<h2 class="mb-4 text-white">Planee su visita este fin de semana</h2>
+						<p class="text-white">
+							Nos gustaría mucho conocerlo este fin de semana durante uno de nuestros servicios:<br>
+							• Domingos: 9 AM<br>
+							Av. Puma Rosa, Edif. Bracho Motors. Punto Fijo, 4101. Edo. Falcón, Venezuela.
+						</p>
+						<a href="planee-visita.php" target="_blank" class="btn btn-primary py-3 px-4">Planee su Visita</a>
+    			</div>
+    		</div>
+    	</div>
+    </section>
+
+@endsection
+
+@section('scripts')
+@endsection
