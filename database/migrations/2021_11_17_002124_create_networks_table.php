9c7635e38bf60b8dff0c83710ef93202af312d48
Simon <rodileduar.simon@gmail.com>
2021-11-17 02:03:31 -0400

    Mentors and Networks

--- database/migrations/2021_11_17_002124_create_networks_table.php
+++ database/migrations/2021_11_17_002124_create_networks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Blog extends Migration
+class CreateNetworksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class Blog extends Migration
      */
     public function up()
     {
-        Schema::create('blog', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('title')->nullable();
-            $table->string('image_header')->nullable();
-            $table->text('content')->nullable();
+        Schema::create('networks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ class Blog extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('networks');
     }
 }
---
+++ database/migrations/2021_11_17_010026_add_migrations_to_network.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMigrationsToNetwork extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('networks', function (Blueprint $table) {
+            //
+            $table->foreignId('mentor_id')->after('name')->nullable()->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->unsignedBigInteger('network_father_id')->after('mentor_id')->nullable();
+            $table->foreign('network_father_id')->references('id')->on('networks')->nullable()->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('network', function (Blueprint $table) {
+            //
+        });
+    }
+}
