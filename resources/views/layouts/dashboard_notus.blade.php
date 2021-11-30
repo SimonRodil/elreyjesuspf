eb6d47d5bfc698179cafee9e7b4f8c553fa9566c
Simon <rodileduar.simon@gmail.com>
2021-11-29 21:37:12 -0400

    Adiso al viejo dashboard

--- resources/views/layouts/dashboard_notus.blade.php
+++ resources/views/layouts/dashboard_notus.blade.php
@@ -1,22 +1,3 @@
-<!--
-
-=========================================================
-* Notus JS - v1.1.0 based on Tailwind Starter Kit by Creative Tim
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/notus-js
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md)
-
-* Tailwind Starter Kit Page: https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
--->
 <!DOCTYPE html>
 <html>
   <head>
@@ -53,7 +34,7 @@
     
   </head>
   <body class="text-blueGray-700 antialiased">
-    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <noscript>Necesita tener javascript para correr esta aplicación.</noscript>
     <div id="root">
       @include('layouts.includes.menu_dashboard')
       <div class="relative md:ml-64 bg-blueGray-50">
